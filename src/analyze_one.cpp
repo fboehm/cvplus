c8caac6c886a1c515dc5be1b31c61bb46cee279f
Frederick Boehm <frederick.boehm@gmail.com>
2022-10-26 15:42:27 -0400

    specified output class

--- src/analyze_one.cpp
+++ src/analyze_one.cpp
@@ -17,20 +17,29 @@
 // reads in the bed file for the corresponding chromosome and calculates the matrix product 
 // genotypes * effects
 
-analyze_one_fold(std::string DBSLMM_output_file, 
+//' Calculate test set residuals for one test set for a single fold
+//' 
+//' @param DBSLMM_output_file file path for a single DBSLMM output file for a single fold
+//' @param bed_file plink bed file path
+//' @param bim_file plink bim file path
+//' @param training_indicator_file file path to a text file containing a single column of 1s and 0s to indicate training set membership for this fold
+//' @param test_indicator_file  file path to a text file containing a single column of 1s and 0s to indicate test set membership for this fold
+//' @param test_pheno_file file path to a text file containing a single column of true phenotype values for the test set for this fold
+//' @return 
+
+arma::vec analyze_one_fold(std::string DBSLMM_output_file, 
                 std::string bed_file, 
                 std::string bim_file,
                 std::string training_indicator_file, 
                 std::string test_indicator_file, 
-                std::string verification_indicator_file){
+                std::string test_pheno_file){
+
     // read indicator files
     std::vector <std::string> training_indic = read_one_column_file(training_indicator_file, "integer");
     std::vector <std::string> test_indic = read_one_column_file(test_indicator_file, "integer");
-    std::vector <std::string> verif_indic = read_one_column_file(verification_indicator_file, "integer");
     // convert to 
     arma::vec training_indices = get_indices(training_indic);
     arma::vec test_indices = get_indices(test_indic);
-    arma::vec verif_indices = get_indices(verif_indic);
     // subset effects vector to have only snps in both DBSLMM output file & bim file
     // we'll also use the resulting indicator vector when reading the bed file
     std::vector<std::vector <std::string> > DBSLMM = read_DSBLMM_output(DBSLMM_output_file); // 3 vectors, rs_id, allele, effect
@@ -44,22 +53,27 @@ analyze_one_fold(std::string DBSLMM_output_file,
     ifstream bed_file_stream(bed_file.c_str(), ios::binary);
     arma::vec geno;
     int DBSLMM_snp = 0;
-    arma::vec subject_indicator = training_indic + verif_indic + test_indic;
+    arma::vec subject_indicator = training_indic + test_indic;
     for (int bim_snp = 0; bim_snp < bim_snp_in_DBSLMM_output.size(); bim_snp++){
         //check if SNP from bim is in DBSLMM file
         if (bim_snp_in_DBSLMM_output[bim_snp]){
             readSNP(bim_snp, subject_indicator, bed_file_stream, geno);
             //partition geno into training, test, and verif sets
             arma::vec training_geno = subset(geno, training_indices);
-            arma::vec verif_geno = subset(geno, verif_indices);
             arma::vec test_geno = subset(geno, test_indices);
             // standardize verif_geno & test_geno
-            arma::vec verif_geno_std = standardize(training_geno, verif_geno);
             arma::vec test_geno_std = standardize(training_geno, test_geno);
             // multiply standardized genotypes by DBSLMM effect for that snp
             
+
+
             DBSLMM_snp++;//advance counter for snps in DBSLMM file
             //note that we assume that snps in DBSLMM file is a subset of snps in bim file 
         }
     }
+    // read in pheno values for test set for this fold
+    std::vector <std::string> test_pheno_string = read_one_column_file(test_pheno_file.c_str());
+    // Calculate residuals
+    arma::vec residuals = test_pheno - pgs;
+    return(residuals);
 }
