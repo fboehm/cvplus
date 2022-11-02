c9c6491e957db568cd6d06cba309ac25811d728f
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-11-02 14:18:23 -0400

    upped wall time

--- shell_scripts/cvplus.sh
+++ shell_scripts/cvplus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --partition=mulan,main
-#SBATCH --time=04:00:00
+#SBATCH --time=24:00:00
 #SBATCH --job-name=cvpluscpp
 #SBATCH --mem-per-cpu=16G
 #SBATCH --output=/net/mulan/home/fredboe/research/ukb-intervals/cluster_outputs/cvpluscpp.out
