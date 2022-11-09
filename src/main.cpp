c750ba6d58f41997a55d24225e0a4939b1e743b0
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-11-09 10:20:37 -0500

    updates to main function

--- src/main.cpp
+++ src/main.cpp
@@ -63,18 +63,16 @@ int main(int argc, char *argv[])
     std::vector<arma::vec> pgs(cPar.n_fold);
     std::vector<arma::vec> v_pgs(cPar.n_fold);
     // specify max block size in number of SNPs
-    uint max_block_size = 10000;pgs[fold] += product_vec;
-    v_pgs[fold] += v_product_vec;
-
-    std::cout << "Starting chr " << chr << std::endl;
+    uint max_block_size = 10000;
+    std::cout << "Starting chr " << cPar.chr_num << std::endl;
     // write args to analyze_one_fold_one_chr with cPar contents
     // set up bed file stream
-    std::string bed_file = cPar.plink_file_prefix + std::to_string(chr) + std::string(".bed");
+    std::string bed_file = cPar.plink_file_prefix + std::to_string(cPar.chr_num) + std::string(".bed");
     std::ifstream bed_file_stream(bed_file.c_str(), std::ios::binary);
-    std::string bim_file_name = cPar.plink_file_prefix + std::to_string(chr) + std::string(".bim");
+    std::string bim_file_name = cPar.plink_file_prefix + std::to_string(cPar.chr_num) + std::string(".bim");
     std::vector<std::string> bim = read_bim_file(bim_file_name); // 1 vector, rs_id
-    std::string training_indicator_file = cPar.path_to_indicator_files + std::string("indicator_training_fold") + std::to_string(fold + 1) + std::string(".txt");
-    std::string test_indicator_file = cPar.path_to_indicator_files + std::string("indicator_test_fold") + std::to_string(fold + 1) + std::string(".txt");
+    std::string training_indicator_file = cPar.path_to_indicator_files + std::string("indicator_training_fold") + std::to_string(cPar.fold_num) + std::string(".txt");
+    std::string test_indicator_file = cPar.path_to_indicator_files + std::string("indicator_test_fold") + std::to_string(cPar.fold_num) + std::string(".txt");
     std::vector<std::string> training_indic_string = read_one_column_file(training_indicator_file);
     std::vector<std::string> test_indic_string = read_one_column_file(test_indicator_file);
     // convert to std::vector <int>
@@ -86,9 +84,9 @@ int main(int argc, char *argv[])
     std::vector<int> test_indices = get_indices(test_indic);
     arma::uvec training_indices_arma = arma::conv_to<arma::uvec>::from(training_indices);
     arma::uvec test_indices_arma = arma::conv_to<arma::uvec>::from(test_indices);
-    test_indices_all_folds[fold] = test_indices_arma;
+    /*test_indices_all_folds[fold] = test_indices_arma;
     training_indices_all_folds[fold] = training_indices_arma;
-    
+    */
     //due to the way I now define true_pheno, in terms of two 
     // vectors - one with (nonmissing) values and the second with 
     // indicator of missingness - I need to make 
@@ -101,10 +99,11 @@ int main(int argc, char *argv[])
     }
     std::vector <int> short_test_indices = get_indices(short_test_indic);
     arma::uvec short_test_indices_arma = arma::conv_to<arma::uvec>::from(short_test_indices);
-    short_test_indices_all_folds[fold] = short_test_indices_arma;
+    /*short_test_indices_all_folds[fold] = short_test_indices_arma;
+    */
     // subset effects vector to have only snps in both DBSLMM output file & bim file
     // we'll also use the resulting indicator vector when reading the bed file
-    std::string dbslmm_output_fn = cPar.dbslmm_output_file_prefix + std::to_string(fold + 1) + std::string("_chr") + std::to_string(chr) + std::string("_best.dbslmm.txt");
+    std::string dbslmm_output_fn = cPar.dbslmm_output_file_prefix + std::to_string(cPar.fold_num) + std::string("_chr") + std::to_string(cPar.chr_num) + std::string("_best.dbslmm.txt");
     std::vector<std::vector<std::string>> DBSLMM = read_DSBLMM_output(dbslmm_output_fn); // 3 vectors, rs_id, allele, effect
     // https://stackoverflow.com/questions/49441588/c-how-to-check-if-contents-of-vector-exist-in-another-vector
     uint n_blocks = ceil(DBSLMM[0].size() /(double) max_block_size);
@@ -132,73 +131,69 @@ ec.n_elem);
         v_pgs[fold].zeros(v_product_vec.n_elem);
     }*/
     
-    // https://stackoverflow.com/pgs[fold] += product_vec;
-    v_pgs[fold] += v_product_vec;
-questions/28607912/sum-values-of-2-vectors
-    //#pragma omp parallel for reduction(+:x,y)
+    // https://stackoverflow.com/questions/28607912/sum-values-of-2-vectors
     //https://stackoverflow.com/questions/11773115/parallel-for-loop-in-openmp
-        
-        
-
-
     for (uint block_num = 0; block_num < n_blocks; block_num++){
-        // set snp_block_size
-        uint snp_block_size;
-        if (block_num + 1 != n_blocks){
-            snp_block_size = max_block_size;
-        } else {
-            snp_block_size = n_snp % max_block_size;
-        }
-        // initialize geno_mat
-        
-        arma::mat geno_mat = arma::zeros<mat>(sum_vec(subject_indicator), snp_block_size);
-        arma::mat test_geno_mat = arma::zeros<mat>(sum_vec(test_indic), snp_block_size);
-        arma::mat verif_geno_mat = arma::zeros<mat>(sum_vec(verification_indic), snp_block_size);
-        arma::vec geno = arma::zeros<vec>(sum_vec(subject_indicator));
-        uint bim_start_point = block_num * max_block_size;
-        uint bim_end_point = bim_start_point + snp_block_size;
-        #pragma omp parallel for num_threads(cPar.thread_num) reduction(+:product_vec,v_product_vec)
-        for (uint bim_snp = bim_start_point; bim_snp < bim_end_point; bim_snp++)
+    // set snp_block_size
+    uint snp_block_size;
+    if (block_num + 1 != n_blocks){
+        snp_block_size = max_block_size;
+    } else {
+        snp_block_size = n_snp % max_block_size;
+    }
+    // initialize geno_mat
+    
+    arma::mat geno_mat = arma::zeros<mat>(sum_vec(subject_indicator), snp_block_size);
+    arma::mat test_geno_mat = arma::zeros<mat>(sum_vec(test_indic), snp_block_size);
+    arma::mat verif_geno_mat = arma::zeros<mat>(sum_vec(verification_indic), snp_block_size);
+    arma::vec geno = arma::zeros<vec>(sum_vec(subject_indicator));
+    uint bim_start_point = block_num * max_block_size;
+    uint bim_end_point = bim_start_point + snp_block_size;
+    #pragma omp parallel for num_threads(cPar.thread_num) reduction(+:product_vec,v_product_vec)
+    for (uint bim_snp = bim_start_point; bim_snp < bim_end_point; bim_snp++)
+    {
+        // check if SNP from bim is in DBSLMM file
+        if (bim_snp_in_DBSLMM_output[bim_snp])
         {
-            // check if SNP from bim is in DBSLMM file
-            if (bim_snp_in_DBSLMM_output[bim_snp])
-            {
-                readSNP(bim_snp, subject_indicator, bed_file_stream, geno);
-                // partition geno into training, test, and verif sets
-                arma::vec training_geno = subset(geno, training_indices_arma);
-                arma::vec test_geno = subset(geno, test_indices_arma);
-                arma::vec verif_geno = subset(geno, verification_indices_arma);
-                arma::vec effects();
-                // standardize verif_geno & test_geno
-                for (uint col = 0; col < geno_mat.n_cols; col++){
-                    //define training_geno, test_geno & verif_geno
-                    arma::vec test_geno_std = standardize(training_geno, test_geno);
-                    arma::vec verif_geno_std = standardize(training_geno, verif_geno);
-                    double dd = std::stod(DBSLMM[2][DBSLMM_snp]);
-                    // put back into matrix
-                    test_geno_mat.col(col) = test_geno_std;
-                    verif_geno_mat.col(col) = verif_geno_std;
-                    effects(col) = dd; 
-                }
-                // multiply standardized genotypes by DBSLMM effect for that snp
-                // multiply the standardized test set genotypes by effect for that snp
-                product_vec += test_geno_mat * effects;
-                v_product_vec += verif_geno_mat * effects;
-                DBSLMM_snp++; // advance counter for snps in DBSLMM file
-                // note that we assume that snps in DBSLMM file is a subset of snps in bim file
+            readSNP(bim_snp, subject_indicator, bed_file_stream, geno);
+            // partition geno into training, test, and verif sets
+            arma::vec training_geno = subset(geno, training_indices_arma);
+            arma::vec test_geno = subset(geno, test_indices_arma);
+            arma::vec verif_geno = subset(geno, verification_indices_arma);
+            arma::vec effects();
+            // standardize verif_geno & test_geno
+            for (uint col = 0; col < geno_mat.n_cols; col++){
+                //define training_geno, test_geno & verif_geno
+                arma::vec test_geno_std = standardize(training_geno, test_geno);
+                arma::vec verif_geno_std = standardize(training_geno, verif_geno);
+                double dd = std::stod(DBSLMM[2][DBSLMM_snp]);
+                // put back into matrix
+                test_geno_mat.col(col) = test_geno_std;
+                verif_geno_mat.col(col) = verif_geno_std;
+                effects(col) = dd; 
             }
+            // multiply standardized genotypes by DBSLMM effect for that snp
+            // multiply the standardized test set genotypes by effect for that snp
+            product_vec += test_geno_mat * effects;
+            v_product_vec += verif_geno_mat * effects;
+            DBSLMM_snp++; // advance counter for snps in DBSLMM file
+            // note that we assume that snps in DBSLMM file is a subset of snps in bim file
         }
-        product_vec_all_blocks += product_vec;
-        v_product_vec_all_blocks += v_product_vec;
-            
+    }
+    product_vec_all_blocks += product_vec;
+    v_product_vec_all_blocks += v_product_vec;
+        
     }
     // store product_vec
-    pgs[fold] += product_vec;
+/*    pgs[fold] += product_vec;
     v_pgs[fold] += v_product_vec;
+*/
+    pgs += product_vec;
+    v_pgs += v_product_vec;
     bed_file_stream.close();
     // loop over folds
 //fix this after my redefinition of true_pheno!
-    residuals_vv[fold] = abs(true_pheno.elem(short_test_indices_all_folds[fold]) - pgs[fold]);
+    residuals_vv = abs(true_pheno.elem(short_test_indices_all_folds) - pgs);
     // assemble residuals_vv into a single arma::vec for all "training + test" subjects
     // initialize resids vector with NaN values
     arma::vec resids(verification_indic.size()); // resids is a vector with one entry per subject in the fam file
@@ -257,82 +252,3 @@ questions/28607912/sum-values-of-2-vectors
     return 0;
 }
 
-void parse_args(int argc, char *argv[], PARAM &cPar)
-{
-    std::string str;
-
-    for (int i = 0; i < argc; ++i)
-    {
-        if (strcmp(argv[i], "--n_fold") == 0)
-        {
-            ++i;
-            str.clear();
-            str.assign(argv[i]);
-            cPar.n_fold = atoi(str.c_str());
-        }
-        else if (strcmp(argv[i], "--dbslmm_output_file_prefix") == 0)
-        {
-            ++i;
-            str.clear();
-            str.assign(argv[i]);
-            cPar.dbslmm_output_file_prefix = str;
-        }
-        else if (strcmp(argv[i], "--plink_file_prefix") == 0)
-        {
-            ++i;
-            str.clear();
-            str.assign(argv[i]);
-            cPar.plink_file_prefix = str;
-        }
-        else if (strcmp(argv[i], "--alpha") == 0)
-        {
-            ++i;
-            str.clear();
-            str.assign(argv[i]);
-            cPar.alpha = std::stod(str.c_str());
-        }
-        else if (strcmp(argv[i], "--path_to_indicator_files") == 0)
-        {
-            ++i;
-            str.clear();
-            str.assign(argv[i]);
-            cPar.path_to_indicator_files = str;
-        }
-        else if (strcmp(argv[i], "--path_to_true_pheno_file") == 0)
-        {
-            ++i;
-            str.clear();
-            str.assign(argv[i]);
-            cPar.path_to_true_pheno_file = str;
-        }
-        else if (strcmp(argv[i], "--outpath") == 0)
-        {
-            ++i;
-            str.clear();
-            str.assign(argv[i]);
-            cPar.outpath = str;
-        }
-                else if (strcmp(argv[i], "--thread_num") == 0)
-        {
-            ++i;
-            str.clear();
-            str.assign(argv[i]);
-            cPar.thread_num = std::stoi(str.c_str());
-        }
-        else if (strcmp(argv[i], "--fold_num") == 0)
-        {
-            ++i;
-            str.clear();
-            str.assign(argv[i]);
-            cPar.fold_num = std::stoi(str.c_str());
-        }
-        else if (strcmp(argv[i], "--chr_num") == 0)
-        {
-            ++i;
-            str.clear();
-            str.assign(argv[i]);
-            cPar.chr_num = std::stoi(str.c_str());
-        }
-    }
-    return;
-}
