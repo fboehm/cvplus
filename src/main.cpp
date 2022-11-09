1a421bd5d6ba68c80b254d0d1b8440eec4423af7
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-11-09 14:46:32 -0500

    moved sections of C++ code from main.cpp to main2.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -38,30 +38,10 @@ int main(int argc, char *argv[])
     castContainer(verification_indic_string, verification_indic);
     std::vector<int> verification_indices = get_indices(verification_indic);
     arma::uvec verification_indices_arma = arma::conv_to<arma::uvec>::from(verification_indices);
-    // define residuals_vv to hold 5 vectors of residuals, one per fold
-    std::vector<arma::vec> residuals_vv(cPar.n_fold);
-    std::vector<arma::uvec> test_indices_all_folds(cPar.n_fold); // holds 5 test index arma vectors. We need these vectors later when we assemble residuals vector for entire "training + test" set
-    std::vector<arma::uvec> training_indices_all_folds(cPar.n_fold);
-    std::vector<arma::uvec> short_test_indices_all_folds(cPar.n_fold);
-    // read true phenos for subsequent use inside folds loop
-    std::vector<std::string> true_pheno_string = read_one_column_file(cPar.path_to_true_pheno_file);
-    //make a vector to indicate missing pheno values, ie "NA", from true_pheno_string
-    std::vector <double> true_pheno_double;
-    std::vector <int> true_pheno_missingness_indicator;
-    for (uint i = 0; i < true_pheno_string.size(); i++){
-        if (true_pheno_string[i] != "NA"){
-            true_pheno_double.push_back(std::stod(true_pheno_string[i]));
-            true_pheno_missingness_indicator.push_back(0);
-        } else {
-            true_pheno_missingness_indicator.push_back(1);
-        }
-    }
-    //make true_pheno_double as a std::vector <double> without missing values
-    // now convert to arma::vec
-    arma::vec true_pheno = arma::conv_to<arma::vec>::from(true_pheno_double);
-
-    std::vector<arma::vec> pgs(cPar.n_fold);
+    
+/*    std::vector<arma::vec> pgs(cPar.n_fold);
     std::vector<arma::vec> v_pgs(cPar.n_fold);
+ */
     // specify max block size in number of SNPs
     uint max_block_size = 10000;
     std::cout << "Starting chr " << cPar.chr_num << std::endl;
@@ -91,16 +71,6 @@ int main(int argc, char *argv[])
     // vectors - one with (nonmissing) values and the second with 
     // indicator of missingness - I need to make 
     // adjusted test_indices_all_folds object
-    std::vector <int> short_test_indic;
-    for (uint subject = 0; subject < test_indic.size(); subject++){
-        if (true_pheno_missingness_indicator[subject] == 0){
-            short_test_indic.push_back(test_indic[subject]);
-        }
-    }
-    std::vector <int> short_test_indices = get_indices(short_test_indic);
-    arma::uvec short_test_indices_arma = arma::conv_to<arma::uvec>::from(short_test_indices);
-    /*short_test_indices_all_folds[fold] = short_test_indices_arma;
-    */
     // subset effects vector to have only snps in both DBSLMM output file & bim file
     // we'll also use the resulting indicator vector when reading the bed file
     std::string dbslmm_output_fn = cPar.dbslmm_output_file_prefix + std::to_string(cPar.fold_num) + std::string("_chr") + std::to_string(cPar.chr_num) + std::string("_best.dbslmm.txt");
@@ -141,10 +111,9 @@ int main(int argc, char *argv[])
         arma::vec geno = arma::zeros<vec>(sum_vec(subject_indicator));
         uint bim_start_point = block_num * max_block_size;
         uint bim_end_point = bim_start_point + snp_block_size;
-        #pragma omp parallel for num_threads(cPar.thread_num) reduction(+:product_vec,v_product_vec)
-        
         //need to iterate bim_snp and DBSLMM_snp in this loop; however
         // want to put DBSLMM_snp in the for () and have bim_snp as an extra counter
+        #pragma omp parallel for num_threads(cPar.thread_num) reduction(+:product_vec,v_product_vec)
         for (uint bim_snp = bim_start_point; bim_snp < bim_end_point; bim_snp++)
         {
             // check if SNP from bim is in DBSLMM file
@@ -155,7 +124,7 @@ int main(int argc, char *argv[])
                 arma::vec training_geno = subset(geno, training_indices_arma);
                 arma::vec test_geno = subset(geno, test_indices_arma);
                 arma::vec verif_geno = subset(geno, verification_indices_arma);
-                arma::vec effects();
+                arma::vec effects(snp_block_size);
                 // standardize verif_geno & test_geno
                 for (uint col = 0; col < geno_mat.n_cols; col++){
                     //define training_geno, test_geno & verif_geno
@@ -175,75 +144,10 @@ int main(int argc, char *argv[])
                 // note that we assume that snps in DBSLMM file is a subset of snps in bim file
             }
         }
-        product_vec_all_blocks += product_vec;
-        v_product_vec_all_blocks += v_product_vec;
             
-    }
-    // store product_vec
-/*    pgs[fold] += product_vec;
-    v_pgs[fold] += v_product_vec;
-*/
-    pgs += product_vec;
-    v_pgs += v_product_vec;
+    } // end loop over blocks
+    
     bed_file_stream.close();
-    // loop over folds
-//fix this after my redefinition of true_pheno!
-    residuals_vv = abs(true_pheno.elem(short_test_indices_all_folds) - pgs);
-    // assemble residuals_vv into a single arma::vec for all "training + test" subjects
-    // initialize resids vector with NaN values
-    arma::vec resids(verification_indic.size()); // resids is a vector with one entry per subject in the fam file
-    resids.fill(datum::nan);
-    for (int fold = 0; fold < cPar.n_fold; fold++)
-    {
-        populate_vec(residuals_vv[fold], test_indices_all_folds[fold], resids);
-    }
-    // resids, from above, still has length equal to one entry per subject in teh fam file.
-    //  Some entries, right now, are still datum::nan
-    //  we need to extract the entries that are not missing into a new vector
-    //  alternatively, we might remove the entries that ARE datum::nan
-    arma::vec resids2 = resids.elem(arma::find_finite(resids));
-    // resids2 then gets added to vector of fitted values
-    //// we already have the five fitted values for each verification set subject ////
-    // need to loop over verif subjects
-    // for each subject, we construct a vector with length equal to the number of "test + training" subjects
-
-    arma::vec upper(verification_indices_arma.n_elem);
-    arma::vec lower(verification_indices_arma.n_elem);
-    for (int v_subject = 0; v_subject < verification_indices_arma.n_elem; v_subject++)
-    {
-        arma::vec v_fitted(verification_indic.size());
-        v_fitted.fill(datum::nan);
-        for (int fold = 0; fold < cPar.n_fold; fold++)
-        {
-            double foo_val = v_pgs[fold](v_subject);
-            arma::vec foo(test_indices_all_folds[fold].n_elem); // resids is a vector with one entry per subject in the fam file
-            foo.fill(foo_val);
-            populate_vec(foo, test_indices_all_folds[fold], v_fitted);
-        } // v_fitted now has nan's in it
-        arma::vec v_fitted2 = v_fitted.elem(arma::find_finite(v_fitted));
-        // calculate fitted + resids vector
-        arma::vec arg_plus = v_fitted2 + resids2;
-        arma::vec ap_sorted = arma::sort(arg_plus, "ascend");
-        arma::vec arg_minus = v_fitted2 - resids2;
-        arma::vec am_sorted = arma::sort(arg_minus, "ascend");
-        // determine quantile using the sorted vector
-        int p_index = ceil((1 - cPar.alpha) * (double) (v_fitted2.n_elem + 1));
-        int m_index = floor(cPar.alpha * (double) (v_fitted2.n_elem + 1));
-        // since c++ indexes start with zero, we subtract 1 when from p_index and m_index to get the correct entries
-        // eg, for the fifth smallest value, we need index 4
-        upper(v_subject) = ap_sorted(p_index - 1);
-        lower(v_subject) = am_sorted(m_index - 1);
-    }
-    // specify filename for true pheno vector for verif subjects
-    std::string true_pheno_outfile = cPar.outpath + std::string("true_pheno.txt");
-    std::string upper_outfile = cPar.outpath + std::string("upper.txt");
-    std::string lower_outfile = cPar.outpath + std::string("lower.txt");
-
-    // subset true phenotype vector to verif subjects
-    arma::vec true_pheno_subset = subset(true_pheno, verification_indices_arma);
-    true_pheno_subset.save(true_pheno_outfile.c_str(), arma_ascii);
-    upper.save(upper_outfile.c_str(), arma_ascii);
-    lower.save(lower_outfile.c_str(), arma_ascii);
     return 0;
 }
 
