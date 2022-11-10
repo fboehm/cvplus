014e6051ce910c6f00b1f222c504bc5833ef9495
Frederick J. Boehm <frederick.boehm@gmail.com>
2022-11-10 11:02:47 -0500

    updated .gitignore

--- src/helpers.hpp
+++ src/helpers.hpp
@@ -31,13 +31,6 @@ std::vector <int> convert_string_to_indices(std::vector <std::string> in_string)
 template<typename C1, typename C2>
 void castContainer(const C1& source, C2& destination);
 
-/*template<typename T, typename T2>
-std::vector<T>& operator<<(std::vector<T>& v, T2 t);
-*/
-
-/*template <typename T>
-std::vector<T> operator+(std::vector<T> lhs, const std::vector<T> & rhs);
-*/
 
 arma::vec populate_vec(arma::vec small_v, arma::uvec indices, arma::vec big_v);
 
